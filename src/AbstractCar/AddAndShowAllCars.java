f1298671b77b19ceea35a39968dcea9db1d6ce23
Robert Szczygielski <robert.szczygielski@wp.pl>
2023-10-19 21:06:40 +0200

    czytelność

--- src/AbstractCar/AddAndShowAllCars.java
+++ src/AbstractCar/AddAndShowAllCars.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class AddAndShowAllCars implements AddAllCarsService, ShowAllCarsService {
 
+    private static String SUV = "suv";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
 
     @Override
     public Map<Integer, BasicCar> addAllCarsService(final int maksiloscAut) {
@@ -13,33 +16,25 @@ public class AddAndShowAllCars implements AddAllCarsService, ShowAllCarsService 
         for (int i = 0; i < maksiloscAut; i++) {
 
             Scanner keyboard = new Scanner(System.in);
-            System.out.println("Wpisz 3 modele aut");
+            System.out.println("Wpisz " + maksiloscAut + " modele aut");
+            System.out.println(ANSI_GREEN);
             System.out.println("podaj typ auta");
-            String suv = "suv";
-            if (keyboard.hasNext(contentEquals(suv))) {
-                keyboard.nextLine();
-                System.out.println("podaj model auta");
-                String modele = keyboard.nextLine();
-                System.out.println(("podaj vin"));
-                int vin = keyboard.nextInt();
-                System.out.println("podaj miles");
-                int miles = keyboard.nextInt();
-                keyboard.nextLine();
-                System.out.println("podaj kolor");
-                String colour = keyboard.nextLine();
+            String carType = keyboard.nextLine();
+            System.out.println("podaj model auta");
+            String modele = keyboard.nextLine();
+            System.out.println(("podaj vin"));
+            int vin = keyboard.nextInt();
+            System.out.println("podaj miles");
+            int miles = keyboard.nextInt();
+            keyboard.nextLine();
+            System.out.println("podaj kolor");
+            String colour = keyboard.nextLine();
+            System.out.println(ANSI_RESET);
+
+            if (carType.equals(SUV)) {
                 BasicCar car = new Suv(modele, vin, miles, colour);
                 carsForRent.put(car.getVin(), car);
             } else {
-                keyboard.nextLine();
-                System.out.println("podaj model auta");
-                String modele = keyboard.nextLine();
-                System.out.println(("podaj vin"));
-                int vin = keyboard.nextInt();
-                System.out.println("podaj miles");
-                int miles = keyboard.nextInt();
-                keyboard.nextLine();
-                System.out.println("podaj kolor");
-                String colour = keyboard.nextLine();
                 System.out.println("podaj max speed");
                 int maxSpeed = keyboard.nextInt();
                 keyboard.nextLine();
